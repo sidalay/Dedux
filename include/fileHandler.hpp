93dac76d3db28bc418047ef26bb4766821ed2a00
Sid Shingaki <sidalaytv@gmail.com>
2023-03-05 19:20:46 -0800

    chore: (WIP) fileHandler declarations

--- include/fileHandler.hpp
+++ include/fileHandler.hpp
@@ -7,8 +7,24 @@
 * Only responsible for retreiving the content.
 * Does not handle PARSING the content (SEE -> parser.hpp).
 * 
-* When dedux is opened fileHandler will run to open the last
-* session worked on (if option is enabled). 
+* When dedux is opened fileHandler will run and attempt to
+* open the last session worked on (if option is enabled). 
 */
+
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+struct FileHandler 
+{
+  std::fstream m_stream{};
+  
+  FileHandler();
+
+  bool LoadProject(std::ifstream input);
+  bool ReadProject();
+  bool WriteProject();
+};
 
 #endif // FILE_HANDLER_HPP
---
+++ src/fileHandler.cpp
@@ -0,0 +1,17 @@
+#include "fileHandler.hpp"
+
+FileHandler::FileHandler() {
+
+}
+
+bool FileHandler::LoadProject(std::ifstream input) {
+  return false;
+}
+
+bool FileHandler::ReadProject() {
+  return false;
+}
+
+bool FileHandler::WriteProject() {
+  return false;
+}
