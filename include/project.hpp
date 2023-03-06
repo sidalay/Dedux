d6ec81e112dc7891511f2d5cab5730cd76ac533d
Sid Shingaki <sidalaytv@gmail.com>
2023-03-05 16:10:45 -0800

    feat: design structure layout
    - project.hpp

---
+++ include/project.hpp
@@ -0,0 +1,13 @@
+#ifndef PROJECT_HPP
+#define PROJECT_HPP
+
+/*
+* Structure that stores all of the information
+* parsed from the .txt file,
+* 
+* File & Project ID
+* Metadata
+* Main Content
+*/
+
+#endif // PROJECT_HPP
