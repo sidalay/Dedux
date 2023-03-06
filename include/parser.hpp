ddd543d5e90d737778f2fb548215f9ab0a1d1e4d
Sid Shingaki <sidalaytv@gmail.com>
2023-03-05 16:10:51 -0800

    feat: design structure layout
    - parser.hpp

---
+++ include/parser.hpp
@@ -0,0 +1,14 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+/*
+* Parse components from .txt file
+* 
+* Project ID
+* Project Title
+* Project Sections?
+* Project Main Content
+* Project Metadata
+*/
+
+#endif // PARSER_HPP
