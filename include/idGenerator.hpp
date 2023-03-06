a7d1fb8e631b4501a870a258d0b850494e6edc75
Sid Shingaki <sidalaytv@gmail.com>
2023-03-05 16:11:02 -0800

    feat: design structure layout
    - idGenerator.hpp

---
+++ include/idGenerator.hpp
@@ -0,0 +1,18 @@
+#ifndef ID_GENERATOR_HPP
+#define ID_GENERATOR_HPP
+
+/*
+* Unique ID allows for project organization
+* for individual use-cases as well as team projects.
+* 
+* Files contain a 2-part unique ID.
+* First half is the unique txt file identifier.
+* Second half is project identifier.
+* 
+* Figure out some kind of unique id hash generating technique
+* to store two 32 uint32's in a 64bit int?
+* 
+* or maybe create two unique string hash id's for first and second half?
+*/
+
+#endif // ID_GENERATOR_HPP
