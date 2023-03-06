c29044ede0598556e7f31a9a1f83bd77a2442c6e
Sid Shingaki <sidalaytv@gmail.com>
2023-03-05 16:11:14 -0800

    feat: design structure layout
    - fileHandler.hpp

---
+++ include/fileHandler.hpp
@@ -0,0 +1,14 @@
+#ifndef FILE_HANDLER_HPP
+#define FILE_HANDLER_HPP
+
+/*
+* Handler for reading/writing to/from .txt files.
+* 
+* Only responsible for retreiving the content.
+* Does not handle PARSING the content (SEE -> parser.hpp).
+* 
+* When dedux is opened fileHandler will run to open the last
+* session worked on (if option is enabled). 
+*/
+
+#endif // FILE_HANDLER_HPP
