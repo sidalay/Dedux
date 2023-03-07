f3b5f6a34a9fcaa9190847270a904fdc023f5cf2
Sid Shingaki <sidalaytv@gmail.com>
2023-03-07 00:13:43 -0800

    chore: update test cases

--- src/dedux.cpp
+++ src/dedux.cpp
@@ -6,10 +6,14 @@ int main() {
   // needs to know the previous session
   FileHandler handler{"projects/testprojec.txt"};
 
-  std::cout << "----------------------------\n";
-  std::cout << (handler.pathFound ? handler.m_buffer : "path does not exist\n");
+  std::cout << "--------------------------\n";
+  std::cout << "|      File Sample 1     |\n";
+  std::cout << "--------------------------\n";
+  std::cout << (handler.GetBuffer() ? handler.GetBuffer().value() : "path does not exist\n\n");
 
-  handler.LoadProject("projects/testproject.txt");
-  std::cout << "\n----------------------------\n";
-  std::cout << (handler.pathFound ? handler.m_buffer : "path does not exist\n");
+  std::cout << "--------------------------\n";
+  std::cout << "|      File Sample 2     |\n";
+  std::cout << "--------------------------\n";
+  handler.LoadFile("projects/testproject.txt");
+  std::cout << (handler.GetBuffer() ? handler.GetBuffer().value() : "path does not exist\n");
 }
